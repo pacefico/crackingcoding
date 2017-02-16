57677e9c514c90b520310c58aaec3532a2a078d8
Paulo Cruz <paulo.Intelitrader@MIDGARD.LOCAL>
2017-02-16 12:04:40 -0200

    adding queue and stacks

---
+++ queue.py
@@ -0,0 +1,76 @@
+"""
+A queue is an abstract data type that maintains the order in which elements were added to it,
+ allowing the oldest elements to be removed from the front and new elements to be added to the rear.
+ This is called a First-In-First-Out (FIFO) data structure because the first element
+ added to the queue (i.e., the one that has been waiting the longest) is always the
+ first one to be removed.
+
+A basic queue has the following operations:
+Enqueue: add a new element to the end of the queue.
+Dequeue: remove the element from the front of the queue and return it.
+
+url: https://www.hackerrank.com/challenges/ctci-queue-using-two-stacks
+"""
+
+class MyQueue(object):
+
+    def __init__(self):
+        self.stack = []
+        self.stack_aux = []
+
+    def switch(self):
+        if len(self.stack_aux) == 0:
+            for item in range(len(self.stack)):
+                self.stack_aux.append(self.stack.pop())
+
+    def peek(self):
+        self.switch()
+        value = self.stack_aux.pop()
+        self.stack_aux.append(value)
+        #print('Print head: {}. Queue:{}. Aux:{}'.format(value, self.stack, self.stack_aux))
+        return value
+
+    def pop(self):
+        self.switch()
+        response = self.stack_aux.pop()
+        #print("Dequeuing:{}. Queue:{}. Aux:{}".format(response, self.stack, self.stack_aux))
+
+    def put(self, value):
+        self.stack.append(value)
+        #print("Enqueue {}. Queue:{}. Aux:{}".format(value, self.stack, self.stack_aux))
+
+def case0():
+    return "1 42:2:1 14:3:1 28:3:1 60:1 78:2:2"
+
+def expect0():
+    return ['14','14']
+
+queue = MyQueue()
+val_list = case0().split(":")
+res_list = []
+for line in val_list:
+    values = line.split(" ")
+    if values[0] == "1":
+        queue.put(values[1])
+    elif values[0] == "2":
+        queue.pop()
+    else:
+        response = queue.peek()
+        res_list.append(response)
+        print(response)
+#print(res_list)
+assert res_list == expect0()
+
+
+#original from input
+# queue = MyQueue()
+# t = int(input())
+# for line in range(t):
+#     values = map(int, input().split())
+#     values = list(values)
+#     if values[0] == 1:
+#         queue.put(values[1])
+#     elif values[0] == 2:
+#         queue.pop()
+#     else:
+#         print(queue.peek())
---
+++ stacks.py
@@ -0,0 +1,50 @@
+"""
+A bracket is considered to be any one of the following characters: (, ), {, }, [, or ].
+
+Two brackets are considered to be a matched pair if the an opening bracket (i.e., (, [, or {)
+occurs to the left of a closing bracket (i.e., ), ], or }) of the exact same type.
+There are three types of matched pairs of brackets: [], {}, and ().
+url: https://www.hackerrank.com/challenges/ctci-balanced-brackets
+"""
+
+import re
+
+
+def is_matched(expression):
+    open_brackets = ["[", "{", "("]
+    close_brackets = ["]", "}", ")"]
+    stack = []
+    for item in expression:
+        if item in open_brackets:
+            stack.append(item)
+        else:
+            if len(stack) == 0 or stack.pop() != open_brackets[close_brackets.index(item)]:
+                return False
+    return True if len(stack) == 0 else False
+
+
+# t = int(input().strip())
+# for a0 in range(t):
+#     expression = input().strip()
+#     if is_matched(expression) == True:
+#         print("YES")
+#     else:
+#         print("NO")
+
+def case0():
+    return "{[()]}"
+
+def case1():
+    return "{[(])}"
+
+def case2():
+    return "{{[[(())]]}}"
+
+def case3():
+    return "{{[[(())]]}}["
+
+#print(is_matched(case2()))
+assert is_matched(case0()) == True
+assert is_matched(case1()) == False
+assert is_matched(case2()) == True
+assert is_matched(case3()) == False
