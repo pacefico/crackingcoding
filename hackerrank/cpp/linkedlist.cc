f15063827d3e2454b29f2fe1baea69caf4bee848
Paulo Figueiredo <pacefico@gmail.com>
2017-03-04 19:49:07 -0300

    ending linkedlistcode c++

--- hackerrank/cpp/linkedlist.cc
+++ hackerrank/cpp/linkedlist.cc
@@ -77,19 +77,51 @@ void case2(Node &node){
   node = *nA;
 }
 
+void case3(Node &node){
+
+  Node *nA = new Node();
+  nA->data = 1;
+  nA->next = NULL;
+
+  node = *nA;
+}
+
+void case4(Node &node){
+
+  Node *nA = new Node();
+  nA->data = 0;
+
+  int N = 100;
+  Node *new_node = new Node();
+  new_node->data = 1;
+  nA->next = new_node;
+  for(int i=2; i <= N; i++) {
+    Node * n = new Node();
+    n->data = i;
+    new_node->next = n;
+    new_node = n;
+  }
+
+  new_node->next = nA->next;
+  node = *nA;
+}
+
 bool has_cycle(Node* head){
   if (head != NULL) {
     Node* node;
     node = head->next;
+    int count = 0;
     while (node != NULL){
-      node = node->next;
-      if (((node == head->next) && (node->data == head->next->data)) || (node == head)){
-         return 1;
+      //printf("count:%d\n", count);
+      if (node == head || node->next == head || count >= 100){
+          return 1;
       }
+      node = node->next;
+      count += 1;
     }
     return 0;
   }
-  return 1;
+  return 0;
 }
 
 const char* bool_cast(const bool b){
@@ -103,12 +135,21 @@ cout << "Linked List" << endl;
 Node node0;
 case0(node0);
 cout << "Case 0: has_link: " << bool_cast(has_cycle(&node0)) << endl;
+
 Node node1;
 case1(node1);
 cout << "Case 1: has_link: " << bool_cast(has_cycle(&node1)) << endl;
+
 Node node2;
 case2(node2);
 cout << "Case 2: has_link: " << bool_cast(has_cycle(&node2)) << endl;
 
-// cout << "case0(): has: " << bool_cast(has_cycle(case0())) << endl;
+Node node3;
+case3(node3);
+cout << "Case 3: has_link: " << bool_cast(has_cycle(&node3)) << endl;
+
+Node node4;
+case4(node4);
+cout << "Case 4: has_link: " << bool_cast(has_cycle(&node4)) << endl;
+
 }
