b49c90b14d6cc07f42d58a334c895d195df186d5
Paulo Figueiredo <pacefico@gmail.com>
2017-03-04 20:47:45 -0300

    adding array rotation in c++

---
+++ hackerrank/cpp/array_left_rotation.cc
@@ -0,0 +1,78 @@
+
+// #include <map>
+// #include <set>
+// #include <list>
+// #include <cmath>
+// #include <ctime>
+// #include <deque>
+// #include <queue>
+// #include <stack>
+// #include <string>
+// #include <bitset>
+// #include <cstdio>
+// #include <limits>
+// #include <climits>
+// #include <cstring>
+// #include <cstdlib>
+// #include <fstream>
+// #include <numeric>
+// #include <sstream>
+// #include <iostream>
+// #include <algorithm>
+// #include <unordered_map>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+vector<int> array_left_rotation(vector<int> a, int n, int k) {
+    for (size_t i = 0; i < k; i++) {
+      vector<int>::iterator val = a.begin();
+      a.push_back(*val);
+      a.erase(a.begin());
+    }
+    for (size_t i = 0; i <= k; i++) {
+      printf("%d ", a[i]);
+    }
+    return a;
+}
+
+void case0(){
+  int values[] = {1,2,3,4,5};
+  int n = 5;
+  int k = 4;
+  vector<int> v(values, values+sizeof(values)/sizeof(int));
+
+  int expected[] = { 5,1,2,3,4 };
+  std::vector<int> new_v = array_left_rotation(v, n, k);
+  for (size_t i = 0; i <= n; i++) {
+    vector<int>::iterator val = new_v.begin() +i;
+    if (*val != expected[i]){
+      printf("assertion error!\n");
+      return;
+    }
+  }
+  printf("assertion OK!\n");
+}
+
+
+int main(){
+  case0();
+}
+
+int main_from_test(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+        cin >> a[a_i];
+    }
+    vector<int> output = array_left_rotation(a, n, k);
+    for(int i = 0; i < n;i++)
+        cout << output[i] << " ";
+    cout << endl;
+    return 0;
+}
