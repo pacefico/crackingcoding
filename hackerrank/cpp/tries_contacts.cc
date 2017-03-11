bb1c0df45619a7a46baa5802139f7b8b84e979d6
Paulo Figueiredo <pacefico@gmail.com>
2017-03-10 23:45:35 -0300

    commiting final result tries

--- hackerrank/cpp/tries_contacts.cc
+++ hackerrank/cpp/tries_contacts.cc
@@ -14,7 +14,7 @@
 // #include <climits>
 // #include <cstring>
 // #include <cstdlib>
-// #include <fstream>
+#include <fstream>
 // #include <numeric>
 // #include <sstream>
 // #include <iostream>
@@ -54,13 +54,13 @@ public:
   TrieNode(){}
 
   TrieNode(char letter){
-    this->count = 1;
+    this->count = 0;
     this->letter = letter;
     this->complete = false;
   }
 
   void add(string item){
-    std::cout << "\nadding: " << item << " on: " << this->letter << '\n';
+    //std::cout << "\nadding: " << item << " on: " << this->letter << '\n';
 
     string::iterator it = item.begin();
     TrieNode *current;
@@ -74,19 +74,18 @@ public:
           //std::cout << "not exist: " << *it << '\n';
           exist = new TrieNode(*it);
           exist->complete = it + 1 == item.end();
+          exist->count ++;
           current->next.insert(pair<char, TrieNode>(*it, *exist));
-          std::cout << "adding: " << *it << " on: " << current->letter << " next size: " << current->next.size() << '\n';
+          //std::cout << "adding: " << *it << " on: " << current->letter << " next size: " << current->next.size() << '\n';
         } else {
-          exist->count += 1;
+          exist->count ++;
           exist->complete = it + 1 == item.end() || exist->complete;
-          std::cout << "exist: " << exist->letter << " c:" << exist->count << " end: " << exist->complete << '\n';
+          //std::cout << "exist: " << exist->letter << " c:" << exist->count << " end: " << exist->complete << '\n';
         }
-
         current = &current->next[*it];
+        exist = NULL;
         ++it;
     }
-    //current = this;
-
   }
 
   TrieNode* getExist(char letter, TrieNode *current){
@@ -100,55 +99,57 @@ public:
   }
 
   int find(string item){
-    std::cout << "\nfinding: " << item << '\n';
+    //std::cout << "\nfinding: " << item << '\n';
     //map<char, TrieNode>::iterator it;
     TrieNode * current;
     current = this;
+    int count = 0;
     string::iterator it = item.begin();
     while(it != item.end()){
       TrieNode * exist;
       exist = getExist(*it, current);
       if (exist != NULL){
         current = &current->next[*it];
-        if (current != NULL && it +1 == item.end()){
-          printf("%c\n",  current->letter);
-          return current->count;
+        if (current != NULL && it +1 == item.end() && current->letter == *it){
+          //printf("-%c-",  current->letter);
+          count = current->count;
+          break;
         }
+      } else {
+        break;
       }
+      exist = NULL;
       ++it;
     }
-
-
-    return 0;
+    return count;
   }
-
 };
 
 class Tries{
-
 public:
   TrieNode *root;
-
   Tries(){
     this->root = new TrieNode('*');
   }
-
   void add(string item){
+    //std::cout << "adding: " << item << endl;
     this->root->add(item);
   }
   int find(string item){
-    return this->root->find(item);
+    //std::cout << "finding: " << item;
+    int response = this->root->find(item);
+    //std::cout << " count: " << response << endl;
+    return response;
   }
-
 };
 
 void case0(){
   std::vector<string> v;
-  v = {   //"add hack",
+  v = {   "add hack",
             "add hac",
             "add ha",
-            "add hackerrank",
             "find hac",
+            "add hackerrank",
             "add hackk",
             "add hackee",
             "find ac",
@@ -173,14 +174,53 @@ void case0(){
       if (values[0] == "add"){
         tries->add(values[1].c_str());
       } else if (values[0] == "find"){
-        printf("%d", tries->find(values[1].c_str()));
+        printf("%d\n", tries->find(values[1].c_str()));
       }
     }
+}
+
+void case1(){
+
+  Tries *tries;
+  tries = new Tries();
+  string line;
+  string line_output;
+  ifstream input_file ("..//artifacts/tries/test2/input.txt");
+  ifstream output_file ("..//artifacts/tries/test2/output.txt");
+  int count = 0;
+  if (input_file.is_open() && output_file.is_open())
+  {
+    while ( getline (input_file,line) )
+    {
+      std::vector<string> values;
+      // if (count == 20){
+      //   break;
+      // }
+
+      values = splitString(line, ' ');
+      if (values[0] == "add"){
+        tries->add(values[1].c_str());
+      } else if (values[0] == "find"){
+        int result = tries->find(values[1].c_str());
+
+        getline (output_file,line_output);
+        if (result != atoi(line_output.c_str())){
+          std::cout << "false: " << count << " expect: " << line_output << '\n';
+          //break;
+        }
+
+      }
+      //cout << line << '\n';
+      count ++;
+
+    }
+    input_file.close();
+  }
 
 }
 
 int main(){
-  case0();
+  case1();
 }
 
 int main_from_test(){
