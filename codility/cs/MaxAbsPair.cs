a1598d267c6010739a9e877a7e9390405dca0654
Paulo Figueiredo <pacefico@gmail.com>
2017-04-08 15:42:57 -0300

    creating code folders by language

---
+++ codility/cs/MaxAbsPair.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaxAbsPair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //simpleSolution();
+            StressTest();
+
+            Console.ReadKey();
+        }
+
+        static void simpleSolution()
+        {
+            List<int> my_list = new List<int>();
+            my_list.Add(1);
+            my_list.Add(3);
+            my_list.Add(-3);
+
+            List<int> my_list2 = new List<int>();
+            my_list2.Add(4);
+            my_list2.Add(3);
+            my_list2.Add(2);
+            my_list2.Add(5);
+            my_list2.Add(1);
+            my_list2.Add(1);
+
+            Console.WriteLine(solution(my_list.ToArray()));
+            Console.WriteLine();
+            Console.WriteLine(solution(my_list2.ToArray()));
+            Console.WriteLine();
+        }
+
+        static int solution(int[] A)
+        {
+            int k = 0;
+            bool nonPair = A.Length % 2 == 0;
+            if (nonPair)
+            {
+                k = A.Length / 2;
+            } else
+            {
+                k = (A.Length / 2) + 1;
+            }
+            //Console.WriteLine(k);
+
+            int maxA = A[0];
+            int maxK = A[k+0-1];
+
+            for (int i = 1; i < k; i++)
+            {
+                int abs_first = Math.Abs(maxA - A[i]);
+                if (abs_first > maxA)
+                {
+                    maxA = abs_first;
+                }
+
+                int abs_second = Math.Abs(maxK - A[k+i-1]);
+
+                if (abs_second > maxK)
+                {
+                    maxK = abs_second;
+                }
+            }
+            return Math.Max(maxA, maxK);
+        }
+
+        static void StressTest()
+        {
+            var rand = new Random();
+            int N = 16;
+            var max_size = 1;
+
+            for (int i = 0; i < N+1; i++)
+            {
+                max_size *= 2;
+                List<int> my_list = new List<int>();
+                Console.WriteLine(String.Format("it: {0} - size: {1}", i, max_size));
+
+                for (int j = 0; j < max_size; j++)
+                {
+                    var value = rand.Next(-1000000000, 1000000000);
+                    my_list.Add(value);
+                }
+
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                int result = solution(my_list.ToArray());
+                watch.Stop();
+                Console.WriteLine(String.Format("solution time: {0} result:{1}", watch.Elapsed, result));
+            }
+        }
+    }
+}
---
+++ codility/cs/ParkingTicketControl.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingTicketControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution("10:00", "13:01"));
+            Console.WriteLine(solution("09:42", "11:42"));
+            Console.WriteLine(solution("00:01", "23:59"));
+            Console.WriteLine(solution("00:01", "00:02"));
+            Console.WriteLine(solution("00:00", "00:59"));
+            Console.WriteLine(solution("00:01", "01:00"));
+            Console.WriteLine(solution("00:01", "01:01"));
+            Console.WriteLine(solution("00:01", "01:02"));
+            Console.WriteLine(solution("00:01", "00:01"));
+            Console.WriteLine(solution("23:58", "00:00"));
+            Console.WriteLine(solution("01:30", "13:40"));
+            Console.ReadKey();
+        }
+
+        static int solution(string E, string L)
+        {
+            /*
+                Entrance: $2
+                First Hour and partial: + $3
+                Next Hour and partial: + $4
+            */
+            DateTime start = DateTime.ParseExact(E, "HH:mm",
+                                        System.Globalization.CultureInfo.InvariantCulture);
+            DateTime end = DateTime.ParseExact(L, "HH:mm",
+                                        System.Globalization.CultureInfo.InvariantCulture);
+            TimeSpan diff = (end - start);
+
+            int totalHours = Convert.ToInt32(diff.Hours);
+            int partialHour = Convert.ToInt32(diff.Minutes);
+
+            //Console.WriteLine(String.Format("partial: {0}", partialHour));
+            //Console.WriteLine(diff);
+            //Console.WriteLine(totalHours);
+
+            if (totalHours < 0 || partialHour < 0)
+            {
+                return 0;
+            }
+            if (partialHour > 0)
+            {
+                totalHours++; // integrate partial hour into totalHours
+            }
+
+            int totalAmount = 2;
+            if (totalHours == 1)
+            {
+                totalAmount += 3;
+            } else if (totalHours > 1)
+            {
+                totalAmount += 3; // for the first hour
+                totalAmount += (totalHours - 1) * 4; // for the successive hours
+            }
+            return totalAmount;
+        }
+    }
+}
