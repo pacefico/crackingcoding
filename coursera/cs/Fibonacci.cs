b46375974fff69ae703f806ade066d09031f25cb
Paulo Figueiredo <pacefico@gmail.com>
2017-04-10 21:49:30 -0300

    adding fibbonnaci and GCD with LeastCommonDivisor

---
+++ coursera/cs/Fibonacci.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class FibonacciHuge
+    {
+        static void Main(string[] args)
+        {
+            //StressTest();
+            ManualTest();
+
+            Console.ReadKey();
+        }
+
+
+        static void ManualTest()
+        {
+            var input_a = Console.ReadLine();
+
+            double result = calc_fib_mod(int.Parse(input_a));
+
+            Console.WriteLine(String.Format("{0}", result % 10));
+        }
+
+        static void StressTest()
+        {
+
+            var rand = new Random();
+            
+            var N = 240;
+            for (int i =3; i < N; i++)
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                double result2 = calc_fib_mod(i);
+                double last_digit = result2 % 10;
+                watch.Stop();
+                var elapsedMs = watch.Elapsed;
+                Console.WriteLine(String.Format("it: {0} calc_fib_mod({1})={2} time: {3} last: {4}", i, i, result2, elapsedMs, last_digit));
+            }
+          
+        }
+
+        static float calc_fib(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            return calc_fib(n - 1) + calc_fib(n - 2);
+        }
+
+        static double calc_fib_faster(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            double n0 = 0;
+            double n1 = 1;
+            double r = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                r = n0 + n1;
+                n0 = n1;
+                n1 = r;
+            }
+            return r;
+        }
+
+        static double calc_fib_mod(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            double n0 = 0;
+            double n1 = 1;
+            double r = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                r = (n0 % 10) + (n1 % 10);
+                n0 = n1;
+                n1 = r;
+            }
+            return r;
+        }
+    }
+
+   
+}
---
+++ coursera/cs/GreaterCommonDivisor.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class GreaterCommonDivisor
+    {
+        static void Main(string[] args)
+        {
+            //StressTest();
+            ManualTest();
+
+            Console.ReadKey();
+        }
+
+        static void ManualTest()
+        {
+            var input_a = Console.ReadLine();
+
+            var numbers = input_a.Split(' ');
+
+            int a = int.Parse(numbers[0]);
+            int b = int.Parse(numbers[1]);
+
+            Console.WriteLine(LCD(a, b));
+        }
+
+        static int GCDNaive(int a, int b)
+        {
+            int current = 1;
+            for (int d = 2; d < Math.Min(a, b) + 1; d++)
+            {
+                if ((a % d == 0) && (b % d == 0))
+                {
+                    if (d > current)
+                    {
+                        current = d;
+                    }
+                }
+            }
+            return current;
+        }
+
+        static double GCDNaiveBig(int a, int b)
+        {
+            double current = 1;
+            for (double d = 2; d < Math.Min(a, b) + 1; d++)
+            {
+                if ((a % d == 0) && (b % d == 0))
+                {
+                    if (d > current)
+                    {
+                        current = d;
+                    }
+                }
+            }
+            return current;
+        }
+
+        static long LCD(long a, long b)
+        {
+            return (a * b) / GCDLemma(a, b);
+        }
+
+        static long GCDLemma(long a, long b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            return GCDLemma(b, a % b);
+        }
+
+        static void StressTest()
+        {
+            var rand = new Random();
+            int N = 1000;
+            
+            int min_value = 1;
+            for (int i = 0; i < N; i++)
+            {
+                min_value *= 2;
+                Console.WriteLine(String.Format("it: {0} min:{1}", i, min_value));
+
+                var a = rand.Next(1, 1000000000);
+                var b = rand.Next(1, 1000000000);
+
+                Console.WriteLine(String.Format("a:{0}, b:{1}", a, b));
+
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                double result = LCD(a, b);
+                watch.Stop();
+                Console.WriteLine(String.Format("LCD time: {0} result:{1}", watch.Elapsed, result));
+
+            }
+        }
+    }
+}
